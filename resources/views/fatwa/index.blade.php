0c1acdfafc27eecd7de4ad3fa424ec0291454442
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-03-25 22:00:25 +0700

    fatwa: index

--- resources/views/fatwa/index.blade.php
+++ resources/views/fatwa/index.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-hero title="Hadis" subtitle="Kumpulan hadis dan terjemahannya dari 9 kitab hadis populer" />
+    <x-hero title="Fatwa DSN" subtitle="Fatwa Dewan Syariah Nasional MUI" />
 
-    <div class="p-4 flex flex-wrap max-w-3xl m-auto">
-        @foreach(range(1,9) as $i)
-            <div class="w-full md:w-1/3 sm:w-1/2 text-center p-2">
-                <div class="bg-gray-100 rounded p-8">
-                    <h2 class="font-bold text-xl">Abu Daud</h2>
-                    <div class="font-light">3.451 hadis</div>
-                </div>
+    <div class="bg-gray-100">
+        <div class="p-4 max-w-3xl m-auto">
+            <div class="bg-white rounded-lg shadow-md">
+                @foreach(range(1,9) as $i)
+                    <div class="flex items-center px-8 py-6 border-b">
+                        <a href="#" class="block flex-grow">
+                            <div class="text-xs tracking-widest">109/DSN-MUI/II/2017</div>
+                            <h2 class="font-bold">Pembiayaan Likuiditas Jangka Pendek Syariah</h2>
+                        </a>
+                        <a href="#" class="ml-8 w-8 h-8 rounded-full bg-pink-100 text-center relative pt-1">
+                            <i class="fal fa-download text-pink-700"></i>
+                        </a>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </div>
     </div>
 @endsection
