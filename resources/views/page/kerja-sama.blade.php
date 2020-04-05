fdca2250a57a201ee6f31023f217acdc8220db26
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-04-05 07:58:17 +0700

    halaman kerjasama

---
+++ resources/views/page/kerja-sama.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <x-hero title="" subtitle=""></x-hero>
+
+    <div class="max-w-3xl m-auto p-8">
+        <div class="shadow-xl rounded bg-white" style="position: relative; margin-top: -4em">
+            <h1 class="font-bold text-3xl text-center leading-tight pattern-wave px-6 py-4">Kerja Sama</h1>
+            <div class="md:text-lg md:leading-normal p-6">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, iure, provident. Optio tempora vitae voluptate. Debitis dignissimos dolore eaque eos laboriosam nobis nostrum provident quia reprehenderit temporibus. Iusto tempore, veniam?
+            </div>
+        </div>
+    </div>
+@endsection
