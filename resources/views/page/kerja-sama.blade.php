4085fb8a163cb144c2c1ccd652748c8cc7df49c8
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-04-05 08:08:00 +0700

    whitespace

--- resources/views/page/kerja-sama.blade.php
+++ resources/views/page/kerja-sama.blade.php
@@ -7,7 +7,7 @@
     <div class="max-w-3xl m-auto p-8">
         <div class="shadow-xl rounded bg-white" style="position: relative; margin-top: -4em">
             <h1 class="font-bold text-3xl text-center leading-tight pattern-wave px-6 py-4">Kerja Sama</h1>
-            <div class="md:text-lg md:leading-normal p-6">
+            <div class="md:text-lg md:leading-normal p-6 md:p-12">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, iure, provident. Optio tempora vitae voluptate. Debitis dignissimos dolore eaque eos laboriosam nobis nostrum provident quia reprehenderit temporibus. Iusto tempore, veniam?
             </div>
         </div>
